dfe61d74d4bc71aeaff80455b64e93538f250ded
Srijan Patel <srijanpatel911@gmail.com>
2025-08-08 16:39:53 +0530

    05:ai-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { useTRPC } from '@/trpc/client'
 import { useMutation } from '@tanstack/react-query'
-import React from 'react'
+import React, { useState } from 'react'
 import { toast } from 'sonner'
 
 const page = () => {
+  const [value,setValue]=useState("")
   const trpc=useTRPC()
   const invoke=useMutation(trpc.invoke.mutationOptions({onSuccess:()=>{
     toast.success("Background Job Started")
@@ -13,7 +15,8 @@ const page = () => {
 
   return (
     <div className='m-64' >
-      <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({text:"srijan is god"})} >Invoke Background Job</Button>
+      <Input value={value} onChange={(e)=>setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({value:value})} >Invoke Background Job</Button>
     </div>
   )
 }
