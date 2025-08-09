08f381b41a7dc4076a03a85eadfca27d11056208
Srijan Patel <srijanpatel911@gmail.com>
2025-08-09 07:07:27 +0530

    06:e2b-sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
 import { success } from "zod";
+import {Sandbox} from "@e2b/code-interpreter"
+
 import { inngest } from "./client";
 import { createAgent, gemini } from '@inngest/agent-kit';
+import { withMaxDuration } from "@trpc/server/unstable-core-do-not-import";
+import { getSandbox } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event ,step}) => {
+    const sandboxId= await step.run("get-sandbox-id",async()=>{
+      const sandbox=await Sandbox.create("vyne-test-2")
+      return sandbox.sandboxId
+    })
 
 const codeAgent = createAgent({
   name: 'code-agent',
@@ -16,8 +24,13 @@ const codeAgent = createAgent({
 const { output } =await codeAgent.run(
   `Write the following snippet:${event.data.value}`,
 );
-console.log(output);
-return {output}
+const sandboxUrl=await step.run("get-sandbox-url",async()=>{
+  const sandbox=await getSandbox(sandboxId)
+  const host=sandbox.getHost(3000)
+  return `https://${host}`
+})
+// console.log(output);
+return {output,sandboxUrl}
 // [{ role: 'assistant', content: 'function removeUnecessaryWhitespace(...' }]
    
   },
